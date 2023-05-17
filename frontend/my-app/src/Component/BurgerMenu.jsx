30cf13c1660ff5ff65ce298423d3ffa87a01830d
Gaurav kumar <ergauravkumar02071998@gmail.com>
2023-05-18 00:23:50 +0530

    full-signup-login

--- frontend/my-app/src/Component/BurgerMenu.jsx
+++ frontend/my-app/src/Component/BurgerMenu.jsx
@@ -1,10 +1,13 @@
 import { 
     Button,
     Box,
-    Heading,
+    
   } from '@chakra-ui/react'
   import { FiAlignJustify } from "react-icons/fi";
+  import { IoClose } from "react-icons/io5";
 import React, {   useState } from 'react'
+import {Link,NavLink } from "react-router-dom"
+
 export function BurgerMenu() {
      const [state,setState]=useState(false)
 
@@ -26,12 +29,12 @@ export function BurgerMenu() {
 
 
       <>
-          <Button onClick={handelOpen} zIndex={100}    position={"absolute" }top="10px" right="10px"><FiAlignJustify size="30"/></Button> 
-          <Box position={"fixed"} zIndex={1} w="100%" h="100vh"   onClick={handelClose} display={state?"block":"none"} >
-                 <Box onClick={handelOpen} zIndex={20}  w="45%" h="100vh" backgroundColor="rgba(168, 170, 187, 0.334)" float="right"   textAlign={"center"} pt="15%">
-                      <Heading>Home</Heading>
-                      <Heading>Signup</Heading>
-                      <Heading>Login</Heading>
+         {!state?<Button onClick={handelOpen} zIndex={100}    position={"absolute" }top="10px" right="10px"><FiAlignJustify size="30"/></Button>:<Button   onClick={handelClose} zIndex={3} position={"absolute" }top="10px" right="10px"><IoClose  size="30"/></Button>  }
+          <Box fontSize={"1.4rem"} position={"fixed"} zIndex={1} w="100%" h="100vh"   onClick={handelClose} display={state?"block":"none"} >
+                 <Box onClick={handelOpen} display={"flex"} flexDirection={"column"} gap="10px" zIndex={20}  w="45%" h="100vh" backgroundColor="rgba(64,123,255,0.5)" float="right"   textAlign={"center"} pt="15%">
+                      <Button  bg="white ">Home</Button>
+                    <NavLink  to="/"  > <Button  p="0rem 9rem" bg="white">Signup</Button></NavLink>
+                    <NavLink  to="/login" > <Button p="0rem 9.4rem" bg="white" >Login</Button></NavLink>
        </Box>         
       </Box>
       
